a0bad8fef83a75b97cca7fc6535fd9a8752f803c
Pål Hamel <pal.hamel@gmail.com>
2020-08-07 17:54:52 +0200

    add const to get element in html

--- public/app.js
+++ public/app.js
@@ -1,2 +1,19 @@
+console.log(firebase);
 
-console.log(firebase)
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById("whenSignedIn");
+const whenSignedOut = document.getElementById("whenSignedOut");
+
+const signInBtn = document.getElementById("signInBtn");
+const signOutBtn = document.getElementById("signOutBtn");
+
+const userDetails = document.getElementById("userDetails");
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+// Sign in event handlers:
+
+
+
+
