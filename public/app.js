dadfd3cb1583c6a5fb6977773bdd5f405fc0bca6
Pål Hamel <pal.hamel@gmail.com>
2020-08-07 21:01:59 +0200

    add state changes and fix error with template literals

--- public/app.js
+++ public/app.js
@@ -1,5 +1,3 @@
-// console.log(firebase);
-
 const auth = firebase.auth();
 
 const whenSignedIn = document.getElementById("whenSignedIn");
@@ -18,3 +16,20 @@ const provider = new firebase.auth.GoogleAuthProvider();
 signInBtn.onclick = () => auth.signInWithPopup(provider);
 
 signOutBtn.onclick = () => auth.signOut();
+// console.log(firebase);
+
+// Def state changes for user:
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // signed in:
+    whenSignedIn.hidden = false;
+    whenSignedOut.hidden = true;
+    userDetails.innerHTML = `<h2>Hello Mr/Mrs ${user.displayName}!</h2> <p>With user ID: ${user.uid} and email: ${user.email}</p>`;
+  } else {
+    // not signed in:
+    whenSignedIn.hidden = true;
+    whenSignedIn.hidden = false;
+    userDetails.innerHTML = '';
+  }
+});
